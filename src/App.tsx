8bc79a7b7c210e0fbc43f2d3de361e845033949e
horouuu <horou@coronne.io>
2025-10-25 10:04:52 +0800

    update toa prog oct 2025

--- src/App.tsx
+++ src/App.tsx
@@ -8,36 +8,31 @@ function App() {
       <div className="text-[24px] font-bold fcorange">
         Tombs of Amascut Gear Progression
       </div>
-      <span className="textrow">April 11 2025</span>
+      <span className="textrow">October 25 2025</span>
       <Section
         title="Core Upgrades"
         description="Start from the left and follow the order."
       >
         <Row
           trail
-          orderString="botd > dpick > rigour > fang > lb & bgs > buckler "
+          orderString="botd > dpick > fang > lb & bgs > rigour > buckler "
         />
-        <Row
-          trail
-          orderString="bbarkt & bbarkb > augury > mbook > anguish > torm > bmoonh"
-        />
-        <Row orderString="obsidianb > feros" />
+        <Row orderString="mbook > anguish > bclaws > torm > feros" />
       </Section>
 
       <Section
         title="Secondary Upgrades"
-        description="Sell items in red to buy the items below them.\n
-        Sell everything past the Virtus mask for the ZCB."
+        description="Sell items in red to buy the items below them."
       >
         <Row
           trail
-          orderString="bclaws > masorih > masorit > masorib > vw $ bclaws"
+          orderString="masorih > masorit > masorib > ayak > conflictiong $ torm > vw $ bclaws"
         />
         <Row
           trail
-          orderString="virtush > virtusb $ bbarkb > virtust $ bbarkt > bmoonb > torture"
+          orderString="torture > rancour $ torture > virtush > virtusb > virtust > bmoonb "
         />
-        <Row orderString="rancour $ torture > fg $ bmoonh > prims > zcb" />
+        <Row orderString="fg > prims " />
       </Section>
 
       <Section
@@ -46,35 +41,29 @@ function App() {
       >
         <Row
           trail
-          orderString="shadow > virtush > eternals > virtusb > virtust > bclaws"
+          orderString="shadow > torm > virtush > eternals > augury > virtusb > virtust"
         />
         <Row
           trail
-          orderString="vw $ bclaws > magus > ancestralh $ virtush > ancestralb $ virtusb > ancestralt $ virtust > bmoonb"
+          orderString="bclaws > vw $ bclaws > magus > conflictiong > ancestralh $ virtush > ancestralb $ virtusb > ancestralt $ virtust"
         />
-        <Row orderString="torture > rancour $ torture > fg > prims > avernic > zcb" />
-      </Section>
-
-      <Section
-        title="500+ Invocation Level No-DDS 3-Down Upgrades"
-        description="Skip if you do not intend on doing 500+ in the near future.\n
-      We strongly recommend getting the infernal cape as it removes the need to buy some Torva pieces.\n
-      Items in blue are useful in OD raids only."
-      >
-        <Row orderString="infernal" />
-        <Row orderString="ultor* > rapier* > torvah $ fg > torvab $ bmoonb > torvat" />
+        <Row
+          trail
+          orderString="bmoonb > torture > rancour $ torture > masorih > masorit > masorib > zcb"
+        />
+        <Row orderString="fg > prims > avernic" />
       </Section>
 
       <Section
         title="Tertiary Upgrades"
         description="Do NOT sell shadow/ancestral pieces for any of these items, including the twisted bow."
       >
-        <Row orderString="torvah $ fg > tbow $ torvat & torvab > torvab $ bmoonb > torvat > zvambs" />
+        <Row orderString="atreads $ magus > tbow > torvab > torvat > torvah > zvambs" />
       </Section>
 
       <Section
         title=""
-        description="Note: Bow of Faerdhinen setups are inferior to Masori + DCB setups.\nThey should only be used by irons or temporarily while obtaining cape upgrades."
+        description="Note: Bow of Faerdhinen setups are inferior to Masori setups.\nThey should only be used by irons or temporarily while obtaining cape upgrades."
       />
     </>
   );
